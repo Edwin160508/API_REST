e08e3f842a518dcfcfc6d149960e05fd2e137f2b
Edwin Lima <edwin.limaa@gmail.com>
2019-03-09 20:47:55 -0300

    Criando middleware de validação de token, usando middleware em projectController.

--- src/index.js
+++ src/index.js
@@ -7,5 +7,9 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+/*Instalando Rotas do tipo Get em app.*/ 
+const projectController = require('./controllers/projectController');
+app.use('/projects', projectController);
+
 require('./controllers/authController')(app);
 app.listen(port);
