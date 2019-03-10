1e02b534e5425de8a542d1458eb68698a02fa0f2
Edwin Lima <edwin.limaa@gmail.com>
2019-03-10 12:52:30 -0300

    Index na pasta controller recebe todos os controllers e adicionar em app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-require('./app/controllers/authController')(app);
-require('./app/controllers/projectController')(app);
-
+require('./app/controllers/index')(app);
 app.listen(port);
