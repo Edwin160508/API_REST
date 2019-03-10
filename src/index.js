504d1aa5fbdc4f045448563c72e070ae03445532
Edwin Lima <edwin.limaa@gmail.com>
2019-03-10 12:44:48 -0300

    Adicionando require projectController no app.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-/*Instalando Rotas do tipo Get em app.*/ 
-const projectController = require('./app/controllers/projectController');
-app.use('/projects', projectController);
 
 require('./app/controllers/authController')(app);
+require('./app/controllers/projectController')(app);
+
 app.listen(port);
